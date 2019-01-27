a5fe481b46f531ddc3124f8c92a8448f5f27bd1b
Your Name <you@example.com>
2019-01-27 22:50:51 +0500

    Cherry-pick develop resolved conflict

--- myshop/settings.py
+++ myshop/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'myshop/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
